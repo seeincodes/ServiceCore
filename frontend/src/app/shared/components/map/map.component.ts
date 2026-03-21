df8f545a055e4433f80dbd05bf6a9794e6adcdfe
Xian Zheng <seeinplay@gmail.com>
2026-03-21 10:46:18 -0500

    task(31): remove clean map style

--- frontend/src/app/shared/components/map/map.component.ts
+++ frontend/src/app/shared/components/map/map.component.ts
@@ -48,11 +48,6 @@ const TILE_STYLES: TileStyle[] = [
     url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
     attribution: '&copy; OpenStreetMap contributors, SRTM | &copy; OpenTopoMap',
   },
-  {
-    name: 'Clean',
-    url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-    attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
-  },
 ];
 
 @Component({
